44cfa37feca16f2eebc9f984e1ee891fa685dd09
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-01 01:11:22 +0300

    home page beatufied

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,7 +1,55 @@
 import React, { useEffect, useRef, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../store/auth";
+import "./HomePage.css";
 
-const TestPage = () => {
-  return <div>HOME</div>;
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const token = useSelector((state) => state.auth.value.myToken);
+
+  const [myData, setMyData] = useState();
+
+  useEffect(() => {
+    getMe();
+  }, []);
+
+  const getMe = () => {
+    fetch("http://192.168.0.10:8080/user/myself", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setMyData(responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const logoutFunction = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
+  return (
+    <div className="home_page">
+      <h1>Welcome To App</h1>
+      <h2>Username: {myData?.username}</h2>
+      {myData && <h4>{JSON.stringify(myData)}</h4>}
+
+      <Button onClick={logoutFunction}>Logout</Button>
+    </div>
+  );
 };
 
-export default TestPage;
+export default HomePage;
